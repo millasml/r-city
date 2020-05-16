de352689dc3cafd633d7273eff0014fd73f83c29
millasml <millasml@yahoo.com.sg>
2020-05-16 13:14:43 +0800

    added styling variables and override bootstrap vars

---
+++ src/components/bottom_nav.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "./bottom_nav.scss"
+
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+export default function BottomNav() {
+  return (
+    <Container className = "bottom-nav">
+      <Row>
+        <Col>Hello</Col>
+        <Col>Hello</Col>
+        <Col>Hello</Col>
+        <Col>Hello</Col>
+        <Col>Hello</Col>
+      </Row>
+    </Container>
+  )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,10 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import BottomNav from './bottom_nav'
+// import "./layout.css"
+import "./layout.scss"
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -34,11 +37,7 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <BottomNav/>
       </div>
     </>
   )
