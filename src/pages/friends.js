2774f63e202b1f7daaa74d66ae39cf88d686cf2e
millasml <millasml@yahoo.com.sg>
2020-05-16 21:09:37 +0800

    friend page

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -1,23 +1,85 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "./friends.scss"
+
+import Image from "react-bootstrap/Image"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import ListGroup from "react-bootstrap/ListGroup"
+
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
+import Profile1 from "../images/profile1.jpg"
+import Profile2 from "../images/profile2.jpg"
+import Profile3 from "../images/profile3.jpg"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChessQueen } from "@fortawesome/free-solid-svg-icons"
+
+const MOCK_FRIENDS = [
+  { name: "Badass", points: "201" },
+  { name: "Badass", points: "201" },
+  { name: "You", points: "201" },
+  { name: "Badass", points: "201" },
+  { name: "Badass", points: "201" },
+  { name: "Badass", points: "201" },
+  { name: "Badass", points: "201" },
+  { name: "Badass", points: "201" },
+]
+
 const IndexPage = () => (
-  <Layout>
+  <Layout headerTitle="Leaderboard">
     <SEO title="Friends" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-      <Image />
-      <Image />
-      <Image />
+    <div className="friends">
+      <div className="top-3">
+        <Row>
+          <Col>
+            <Image className="irrelevant" src={Profile1} roundedCircle fluid />
+            <div className="rank">2</div>
+            <div className="label">Milla</div>
+            <hr></hr>
+            <div className="label">
+              <FontAwesomeIcon icon={faChessQueen} /> 305
+            </div>
+          </Col>
+          <Col xs={5}>
+            <Image className="first" src={Profile2} roundedCircle fluid />
+            <div className="rank">1</div>
+            <div className="label">Brandon</div>
+            <hr></hr>
+            <div className="label">
+              <FontAwesomeIcon icon={faChessQueen} /> 305
+            </div>
+          </Col>
+          <Col>
+            <Image className="irrelevant" src={Profile3} roundedCircle fluid />
+            <div className="rank">3</div>
+            <div className="label">Celeste</div>
+            <hr></hr>
+            <div className="label">
+              <FontAwesomeIcon icon={faChessQueen} /> 305
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <ListGroup>
+        {MOCK_FRIENDS.map((friend, rank) => {
+          return (
+            <ListGroup.Item>
+              <Row>
+                <Col xs={2}><div className="rank">{rank + 4}</div></Col>
+                <Col xs={7}>{friend.name}</Col>
+                <Col>
+                  <FontAwesomeIcon icon={faChessQueen} /> {friend.points}
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          )
+        })}
+      </ListGroup>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
